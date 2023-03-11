68b88f6ca3e5d2d1993507906f82363b8ec2b6cf
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-03-11 10:34:30 +0100

    Eslint and prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { OrganizationsModule } from './organizations/organizations.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), OrganizationsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
