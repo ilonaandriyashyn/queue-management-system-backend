d6be7f7eb48602d55fede6cf48d88b74ff469165
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-03-11 21:11:22 +0100

    Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { OrganizationsModule } from './organizations/organizations.module'
-import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), OrganizationsModule],
+  imports: [
+    OrganizationsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'queue',
+      synchronize: true, // Turn this off in production
+      autoLoadEntities: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
