e129c344ff6df03e717d13f0414bb08882262280
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-04-19 08:57:43 +0200

    Removed TODO

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -143,7 +143,6 @@ export class TicketsService {
       .getCount()
   }
 
-  // TODO maybe refactor to query builder
   async findNextByServices(services: Service[]) {
     const servicesIds = services.map((service) => service.id)
     const tickets = await this.ticketsRepository.find({
